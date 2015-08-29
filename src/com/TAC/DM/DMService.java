0c3ec450049ae10b0d505f92861924e2e0668fbb
wzyddg <haloilove@gmail.com>
2015-08-29 21:41:26 +0800

    a mini prototype , communicate ok . adding hibernate

--- src/com/TAC/DM/DMService.java
+++ src/com/TAC/DM/DMService.java
@@ -1,5 +1,66 @@
 package com.TAC.DM;
 
-public class DMService {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
 
+public class DMService implements Runnable {
+	private Socket clientSocket;
+	private BufferedReader in;
+	private PrintWriter out;
+
+	public DMService(Socket serviceSocket) {
+		this.clientSocket = serviceSocket;
+	}
+
+	public String changeCharset(String str, String oldCharset, String newCharset)
+			throws UnsupportedEncodingException {
+		if (str != null) {
+			// 用旧的字符编码解码字符串。解码可能会出现异常。
+			byte[] bs = str.getBytes(oldCharset);
+			// 用新的字符编码生成字符串
+			return new String(bs, newCharset);
+		}
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			// TODO Auto-generated method stub
+			System.out.println("---start Service----");
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
+
+			while (true) {
+				String str = in.readLine();
+				if (str != null) {
+					System.out.println("Request:" + str);
+					String aString = "aasdf";
+					out.println("backdfds");
+					out.flush();
+				} else { // closeconnection
+					System.out.println("read nulg and out");
+					break;
+				}
+			}
+			out.close();
+			in.close();
+			clientSocket.close();
+
+			// Close the connection, but not the server socket
+			System.out.println("--end service--");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+
+		}
+	}
 }
