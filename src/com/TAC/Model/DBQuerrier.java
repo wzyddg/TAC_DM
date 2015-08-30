31d61e22de2de93a3cfdf97f34c098b2c594ec89
wzyddg <haloilove@gmail.com>
2015-08-30 13:43:06 +0800

    hibernate fixed

---
+++ src/com/TAC/Model/DBQuerrier.java
@@ -0,0 +1,29 @@
+package com.TAC.Model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class DBQuerrier {
+	static SessionFactory sessionFactory;
+	static Configuration configuration;
+
+	private static Session sessionStart() {
+		if (sessionFactory == null) {
+			configuration = new Configuration().configure();
+			StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties()).build();
+			sessionFactory = configuration
+					.buildSessionFactory(standardServiceRegistry);
+		}
+		return (Session) sessionFactory.openSession();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
